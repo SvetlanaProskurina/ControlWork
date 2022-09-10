958f5bed5808c38fac14337fe2bf501c1d2e1b81
Svetlana Proskurina <amourka@yandex.ru>
2022-09-10 23:01:06 +0300

    add program

---
+++ program/Program.cs
@@ -0,0 +1,56 @@
+﻿// Console.WriteLine("Hello, World!");
+
+void PrintArray(string[] inputArray)
+{
+
+    int i = 0;
+    while (i < inputArray.Length)
+    {
+        if (i < inputArray.Length && i != inputArray.Length - 1)
+        {
+            Console.Write($"{inputArray[i]} ,");
+        }
+        else Console.Write($"{inputArray[i]}");
+        i++;
+    }
+}
+
+string[] MassiveNew(string[] MassiveInput) //massive with string elements which have length less then 3
+{
+    int LengthMassiveOut = 0;
+
+    for (int i = 0; i < MassiveInput.Length; i++)
+    {
+        if (MassiveInput[i].Length < 3)
+        {
+            LengthMassiveOut++;
+        }
+    }
+
+    string[] MassiveOutput = new string[LengthMassiveOut];
+    int j = 0;
+
+    for (int i = 0; i < MassiveInput.Length; i++)
+    {
+        if (MassiveInput[i].Length < 3)
+        {
+            MassiveOutput[j] = MassiveInput[i];
+            j++;
+        }
+    }
+
+    return MassiveOutput;
+}
+
+//string [] MassiveInput = new string [] { "hello", "2", "word", ":-)" };
+Console.WriteLine("введите массив из строк через запятую");
+string input = Console.ReadLine();
+string[] MassiveInput = input.Split(',');
+Console.Write("Исходный массив: [");
+PrintArray(MassiveInput);
+Console.Write(" ]");
+Console.WriteLine("");
+Console.Write("Массив с элементами, длина которых меньше трёх: ");
+Console.Write("[ ");
+PrintArray(MassiveNew(MassiveInput));
+Console.WriteLine(" ]");
